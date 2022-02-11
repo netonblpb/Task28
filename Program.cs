ad464688586135e976523291ad488953f467ed66
netonblpb <skullbog@gmail.com>
2022-02-11 15:32:56 +0300

    Task28 complete

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.Write("Ввведите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int summ = 0;
+while (number >0)
+{
+    summ += number%10;
+    number = number/10;
+}
+
+Console.WriteLine($"Сумма цифр - {summ}");
